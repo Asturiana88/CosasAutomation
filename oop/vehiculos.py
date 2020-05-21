aa46688a80d88479839b6eb3430578cc92d4238f
Asturiana88 <julietaorce@gmail.com>
2020-05-21 11:40:27 +0100

    Cosis mas

--- oop/vehiculos.py
+++ oop/vehiculos.py
@@ -8,9 +8,12 @@ class Automovil():
         print(f"La velocidad actual es de {self.velox_actual} de {self.velocidad_max}")
     def acelerar(self, velocidad):
         self.velox_actual = self.velox_actual + velocidad
+        self.mostrar_velocimetro()
     def frenar(self, velocidad):
         self.velox_actual = self.velox_actual - velocidad
+        self.mostrar_velocimetro()
 
 mi_auto = Automovil('verde','fiat',180)
 print(mi_auto.color)
-mi_auto.mostrar_velocimetro()
+mi_auto.acelerar(80)
+mi_auto.frenar(50)
